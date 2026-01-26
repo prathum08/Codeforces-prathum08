f8e6f6445e3ef577aba5fac811c1ad5cb5faf905
prathum08 <prathumbhangadia08@gmail.com>
2026-01-26 23:36:16 +0530

    LeetCode

---
+++ leetcode_1200.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        int n = arr.size();
+        sort(arr.begin() , arr.end());
+        vector<vector<int>> result;
+        int minDiff = INT_MAX;
+        for(int i = 1 ; i < n ; i++){
+            if(minDiff > abs(arr[i] - arr[i - 1])){
+                minDiff = abs(arr[i] - arr[i - 1]);
+            }
+        }
+        for(int i = 1;  i < n ; i++){
+            vector<int> ex;
+            if(abs(arr[i] - arr[i - 1]) == minDiff){
+                ex.push_back(arr[i - 1]);
+                ex.push_back(arr[i]);
+            }
+            if(!ex.empty()){
+                result.push_back(ex);
+            }
+        }
+
+        return result;
+    }
+};
+
+// 26-1-26
