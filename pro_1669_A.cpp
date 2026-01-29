ac32e50cfa9d4785cd8d398abc753c9f3aee589a
prathum08 <prathumbhangadia08@gmail.com>
2026-01-29 20:01:04 +0530

    CodeForces

---
+++ pro_1669_A.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    while(n--){
+        int rating;
+        cin >> rating;
+        if(rating >= 1900){
+            cout << "Division 1" << endl;
+        }
+        else if(rating >= 1600 && rating <= 1899){
+            cout << "Division 2" << endl;
+        }
+        else if(rating >= 1400 && rating <= 1599){
+            cout << "Division 3" << endl;
+        }
+        else{
+            cout << "Division 4" << endl;
+        }
+        
+    }
+}
+
+// 29-1-26
