2c483b236f674544ed1a43a43032ff16391d7b4b
prathum08 <prathumbhangadia08@gmail.com>
2026-02-03 22:56:47 +0530

    Leetcode

---
+++ leetcode_3637.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+       int n = nums.size();
+       int i = 0;
+        // first checking increasing
+        while(i + 1 < n && nums[i] < nums[i + 1]){
+            i++;
+        } 
+        if(i == 0 || i == n - 1){
+            return false;
+        }
+
+        //decreasing
+        while(i + 1 < n && nums[i] > nums[i + 1]){
+            i++;
+        }
+        if(i == n - 1){
+            return false;
+        }
+
+        // increasing
+        while(i + 1 < n && nums[i] < nums[i + 1]){
+            i++;
+        }
+
+        return i == n - 1;
+    }
+};
+
+// 3 - 2  - 26
